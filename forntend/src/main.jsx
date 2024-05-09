a3e5843b0a52e1e0ffaac514bfbcdbeb7c182283
harshshrivastava15 <harshshrivastava.work@gmail.com>
2024-05-09 19:33:20 +0530

    edited frontend

--- forntend/src/main.jsx
+++ forntend/src/main.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import ReactDOM from 'react-dom/client';
 
 import App from './App.jsx';
 import './index.css';
@@ -14,3 +13,4 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     </SnackbarProvider>
   </BrowserRouter>
 );
+
